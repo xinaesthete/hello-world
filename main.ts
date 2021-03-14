ad5217dea9a0e247d8d0750c9a5589cf5e5a3e5a
Peter Todd <xinaesthete@users.noreply.github.com>
2021-03-14 17:03:43 +0000

    sound

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    music.ringTone(randint(131, 988))
+})
+let a = 0
 let freq = 0
 let dt = 0
 let phase = 0
 let t = input.runningTime()
+music.ringTone(196)
 basic.forever(function () {
     dt = input.runningTime() - t
-    freq = Math.map(input.acceleration(Dimension.Z), -1023, 1023, 0.1, 4)
+    freq = Math.map(input.acceleration(Dimension.Z), -1023, 1023, 0.1, 20)
     phase += freq * (dt / 3142)
     t = input.runningTime()
+    a = Math.abs(Math.sin(phase))
+    music.setVolume(a * 127)
     led.plotBarGraph(
-    Math.abs(Math.sin(phase)),
+    a,
     1.2
     )
 })
