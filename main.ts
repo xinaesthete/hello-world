18041871a32d19d2f825b9ff9525f114ff6b8cd2
Peter Todd <xinaesthete@users.noreply.github.com>
2021-03-14 17:27:33 +0000

    waveshape

--- main.ts
+++ main.ts
@@ -6,13 +6,13 @@ let freq = 0
 let dt = 0
 let phase = 0
 let t = input.runningTime()
-music.ringTone(196)
+music.ringTone(131)
 basic.forever(function () {
     dt = input.runningTime() - t
-    freq = Math.map(input.acceleration(Dimension.Z), -1023, 1023, 0.1, 20)
+    freq = Math.map(input.acceleration(Dimension.Z), -1024, 1024, 0.1, 10)
     phase += freq * (dt / 3142)
     t = input.runningTime()
-    a = Math.abs(Math.sin(phase))
+    a = Math.abs(Math.sin(phase)) ** 100
     music.setVolume(a * 127)
     led.plotBarGraph(
     a,
